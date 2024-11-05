f6e81d0e7d7d37ea98c26137040ba2721ad5147c
Aslihan Aria Celik <aslihancelik27@gmail.com>
2024-11-04 22:50:41 -0500

    Add project files.

---
+++ BinarySearchRunTimeAnalysis.cpp
@@ -0,0 +1,93 @@
+// BinarySearchRunTimeAnalysis.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+// Recursive Binary Search Function
+int recursiveBinarySearch(const vector<int>& vec, int left, int right, int target) {
+    if (left > right) {
+        return -1;  // Target not found
+    }
+    int mid = left + (right - left) / 2;
+    if (vec[mid] == target) {
+        return mid;
+    }
+    else if (vec[mid] > target) {
+        return recursiveBinarySearch(vec, left, mid - 1, target);
+    }
+    else {
+        return recursiveBinarySearch(vec, mid + 1, right, target);
+    }
+}
+
+// Iterative Binary Search Function
+int iterativeBinarySearch(const vector<int>& vec, int target) {
+    int left = 0;
+    int right = vec.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (vec[mid] == target) {
+            return mid;
+        }
+        else if (vec[mid] > target) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return -1;  // Target not found
+}
+
+// Sequential Search Function
+int sequentialSearch(const vector<int>& vec, int target) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == target) {
+            return i;
+        }
+    }
+    return -1;  // Target not found
+}
+
+int main() {
+    std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int target = 6;
+    int result = recursiveBinarySearch(vec, 0, vec.size() - 1, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    }
+    else {
+        cout << "Element not found." << endl;
+    }
+
+    target = 5;
+    result = iterativeBinarySearch(vec, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    }
+    else {
+        cout << "Element not found." << endl;
+    }
+
+    target = 7;
+    result = sequentialSearch(vec, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    }
+    else {
+        cout << "Element not found." << endl;
+    }
+
+    return 0;
+}
